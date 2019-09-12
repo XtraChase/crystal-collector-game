e6652a113c2f4cad55600095bb2a97943d546337
Chase Laptop <james.chase.richards@gmail.com>
2019-09-12 16:42:44 -0400

    jquery elements added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,44 @@ var yourNumber = []; // total user input amount
 
 //function to start a new round
 function startNewRound() {
-  randomNum = Math.floor(Math.random() * 120) + 19; //new number to achieve is generated
+  randomNum = Math.floor(Math.random() * 101) + 19; //new number to achieve is generated
   btn1 = Math.floor(Math.random() * 12) + 1; //new button number generated
   btn2 = Math.floor(Math.random() * 12) + 1; //new button number generated
   btn3 = Math.floor(Math.random() * 12) + 1; //new button number generated
   btn4 = Math.floor(Math.random() * 12) + 1; //new button number generated
-  yourNumber = 0; //total score is reset
+  yourNumber = []; //your number is reset
+  $(".given-number").text(randomNum);
+  $(".button1").val(btn1);
+  $(".button2").val(btn2);
+  $(".button3").val(btn3);
+  $(".button4").val(btn4);
+  $("#totalNum").text();
 }
+
+//  ********* Gameplay *********
+$(".button").on("click", function() {
+  var buttonPressed = $(this).val();
+  result = $("#totalNum").text((yourNumber += buttonPressed));
+  console.log("Button pressed =" + " " + buttonPressed);
+
+  if (yourNumber === randomNum) {
+    startNewRound();
+    win++;
+    $("#winAmount").text(win);
+    console.log("WIN");
+  } else if (yourNumber > randomNum) {
+    startNewRound();
+    lose++;
+    $("#loseAmount").text(lose);
+    console.log("LOSE");
+  }
+});
+
+//Set CSS
+$(".given-number").css({
+  fontSize: "4em",
+  color: "azure",
+  margin: "50px 50px 0px 0px"
+});
+
+$(".total-amount").css({ fontSize: "2em", color: "azure" });
